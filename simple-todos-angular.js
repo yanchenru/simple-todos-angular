5f5e1c39e1d140a5f36d498792602ba0b8b9893e
Chenru Yan <yanchenru@gmail.com>
2016-02-12 16:20:42 -0700

    missing last two sections

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -1,23 +1,52 @@
+Tasks = new Mongo.Collection('tasks');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_ONLY"
+    });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
+    angular.module('simple-todos', ['angular-meteor', 'accounts.ui']);
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    angular.module('simple-todos').controller('TodosListCtrl', ['$scope', '$meteor',
+    function ($scope, $meteor) {
+
+            $scope.tasks = $meteor.collection(function () {
+                return Tasks.find($scope.getReactively('query'), {
+                    sort: {
+                        createdAt: -1
+                    }
+                })
+            });
+
+            $scope.addTask = function (newTask) {
+                $scope.tasks.push({
+                        text: newTask,
+                        createdAt: new Date(), // current time
+                        owner: Meteor.userId(), // _id of logged in user
+                        username: Meteor.user().username
+                    } // username of logged in user
+                );
+            };
+
+            $scope.$watch('hideCompleted', function () {
+                if ($scope.hideCompleted)
+                    $scope.query = {
+                        checked: {
+                            $ne: true
+                        }
+                    };
+                else
+                    $scope.query = {};
+            });
+
+            $scope.incompleteCount = function () {
+                return Tasks.find({
+                    checked: {
+                        $ne: true
+                    }
+                }).count();
+            };
+
+    }]);
 }
